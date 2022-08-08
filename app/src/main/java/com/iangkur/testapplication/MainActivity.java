485a884efb5b0c487ba7089172d475822816a872
Angkur Mondal <mondal.angkur@gmail.com>
2022-08-08 16:17:54 +0600

    updated in MainActivity file

--- app/src/main/java/com/iangkur/testapplication/MainActivity.java
+++ app/src/main/java/com/iangkur/testapplication/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i("TAG", "Message");
+
     }
 
     public void buttonOnClicked(View view) {
